97ffde6b755c157723ba4a6a4022b6be20bc7310
Fabrice Gobbo <fab@hestia.local>
2018-04-04 23:01:00 +0200

    ajout

--- getInkLevel_sql.sh
+++ getInkLevel_sql.sh
@@ -22,34 +22,12 @@
 declare -a capacity
 declare -a levelInk
 
-<<COMMENT1
-# T3500
-ip='192.168.150.34'
-for i in {1..6}
-do
-    capacity[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.8.1.$i | cut -d' ' -f2)
-    levelInk[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.9.1.$i | cut -d' ' -f2)
-done
-
-# T930
-ip='192.168.150.35'
-for i in {1..6}
-do
-    capacity[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.8.1.$i | cut -d' ' -f2)
-    levelInk[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.9.1.$i | cut -d' ' -f2)
-done
-COMMENT1
-
 ip='192.168.150.19'
 for i in {1..4}
 do
-    capacity[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.8.1.$i | cut -d' ' -f2)
     levelInk[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.9.1.$i | cut -d' ' -f2)
 done
 
-#echo ${capacity[*]} | sed -e 's/ /,/g'
-val='Develop 1c'$(${levelInk[*]} | sed -e 's/ /,/g')'0,0,0,0,0,0,0,0'
-#echo ${levelInk[*]} | sed -e 's/ /,/g'
+val2=$(echo ${levelInk[*]} | sed -e 's/ /,/g'),0,0,0,0,0,0,0,0
 
-
-sqlite3 log_plotter_ink.sqlite 'INSERT ink_logs VALUES($val)'
+sqlite3 log_plotter_ink.sqlite 'INSERT INTO ink_logs ('printer','l_pk','l_g','l_mk','l_c','l_m','l_y','c_pk','c_g','c_mk','c_c','c_m','c_y') VALUES("Develop 1c",'$val2')'
