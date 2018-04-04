5c0df0f5bd4660ecdfd3f67c6237d56cdc59b3d0
gowitz <fab@ouranos.local>
2018-04-04 21:45:25 +0200

    maj DB et getInkLevel_sql.sh

--- getInkLevel_sql.sh
+++ getInkLevel_sql.sh
@@ -22,18 +22,34 @@
 declare -a capacity
 declare -a levelInk
 
+<<COMMENT1
 # T3500
-ip='192.168.150.19'
+ip='192.168.150.34'
 for i in {1..6}
 do
-    #capacity[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.8.1.$i | cut -d' ' -f2)
-    #levelInk[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.9.1.$i | cut -d' ' -f2)
+    capacity[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.8.1.$i | cut -d' ' -f2)
+    levelInk[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.9.1.$i | cut -d' ' -f2)
+done
+
+# T930
+ip='192.168.150.35'
+for i in {1..6}
+do
+    capacity[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.8.1.$i | cut -d' ' -f2)
+    levelInk[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.9.1.$i | cut -d' ' -f2)
+done
+COMMENT1
+
+ip='192.168.150.19'
+for i in {1..4}
+do
+    capacity[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.8.1.$i | cut -d' ' -f2)
     levelInk[${i}]=$(snmpwalk -v 2c -c public -Oq $ip iso.3.6.1.2.1.43.11.1.1.9.1.$i | cut -d' ' -f2)
 done
 
 #echo ${capacity[*]} | sed -e 's/ /,/g'
-echo ${levelInk[*]} | sed -e 's/ /,/g'
+val='Develop 1c'$(${levelInk[*]} | sed -e 's/ /,/g')'0,0,0,0,0,0,0,0'
+#echo ${levelInk[*]} | sed -e 's/ /,/g'
 
-# T930
 
-#sqlite3 printers.sqlite 'UPDATE printers_colors SET ink_level='$((100 * tl / cc))' WHERE printers_id=9 AND colors_id=5'
+sqlite3 log_plotter_ink.sqlite 'INSERT ink_logs VALUES($val)'
